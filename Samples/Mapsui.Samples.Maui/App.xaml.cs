f149bb322396c5fa20c8dfc40e71cff84433a6a5
Janus Weil <janus@gcc.gnu.org>
2025-07-20 12:36:45 +0200

    Mapsui.Samples.Maui: reproduce SkiaSharp issue 2550
    * to reproduce the bug in the context of MAUI 9, see
      https://github.com/mono/SkiaSharp/issues/2550#issuecomment-3094137132
    * notes:
      - only happens on Android
      - requires UseGPU = true
      - only happens with MAUI > 9.0.51
      - if you navigate "Map -> Tab2 -> Tab3 -> Map",
        then the map does not show up any more

--- Samples/Mapsui.Samples.Maui/App.xaml.cs
+++ Samples/Mapsui.Samples.Maui/App.xaml.cs
@@ -5,6 +5,7 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        Mapsui.UI.Maui.MapControl.UseGPU = true;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
